a2a44034c80981efa58c595e0d03f0295841a8b1
Mark <mark_09@inbox.ru>
2021-04-09 09:11:11 +0300

    add work 3 file

---
+++ GeekBrains_AlgorithmsAndDataStructures/HomeWorkNumber3.c
@@ -0,0 +1,54 @@
+//Коротких Марк Александрович
+
+/// <summary>1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций сравнения оптимизированной и не оптимизированной программы. Написать функции сортировки, которые возвращают количество операций.</summary>
+/// <returns></returns>
+void Solution_3_1() {
+
+}
+
+/// <summary>2. *Реализовать шейкерную сортировку.</summary>
+/// <returns></returns>
+void Solution_3_2() {
+
+}
+
+/// <summary3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный массив. Функция возвращает индекс найденного элемента или -1, если элемент не найден.</summary>
+/// <returns></returns>
+void Solution_3_3() {
+
+}
+
+/// <summary>4. *Подсчитать количество операций для каждой из сортировок и сравнить его с асимптотической сложностью алгоритма.</summary>
+/// <returns></returns>
+void Solution_3_4() {
+
+}
+
+int StartHomeWorkNumber3()
+{
+
+    int _numberTask;
+
+    _numberTask = GetInt("Enter the task number(1-4): ", 1, 1, 1, 3);
+
+    switch (_numberTask)
+    {
+    /*case 1:
+        Solution_3_1();
+        break;
+    case 2:
+        Solution_3_2();
+        break;
+    case 3:
+        Solution_3_3();
+        break;
+    case 4:
+        Solution_3_4();
+        break;*/
+    default:
+        printf("This task has not yet been completed :(");
+        break;
+    }
+
+    return 1;
+}
